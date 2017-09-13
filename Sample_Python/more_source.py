7f276cf4fd31b6d211f4ceba9de42c6002f0d159
Kyle Miller <millro04@luther.edu>
2017-09-12 19:29:39 -0500

    Initial commit

--- Sample_Python/more_source.py
+++ Sample_Python/more_source.py
@@ -1,4 +1,7 @@
  def main():
     print("Hello World!")
+    print("git clone") #cloning the repo from github
+    print("git branch KyleMiller") #Makes a branch named KyleMiller
+    print("git checkout KyleMiller") #check out KyleMiller branch to work on that branch
     
 main()
